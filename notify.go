87d557a8eaaa7e34cd434ac57707c4b81fd8da2d
Oliver Herms <oliver.herms@exaring.de>
2018-03-15 11:32:49 +0100

    Improving format string

--- notify.go
+++ notify.go
@@ -88,7 +88,7 @@ func (r *Receiver) Notify(data *alertmanager.Data) (bool, error) {
 	// Add Labels
 	if r.conf.CopyPromLabels {
 		for k, v := range data.GroupLabels {
-			issue.Fields.Labels = append(issue.Fields.Labels, fmt.Sprintf("%s=\"%s\"", k, v))
+			issue.Fields.Labels = append(issue.Fields.Labels, fmt.Sprintf("%s=\"%q\"", k, v))
 		}
 	}
 
