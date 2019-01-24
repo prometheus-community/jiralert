00f096fd89b6a74c658d30b93a2fe0ee0c53aaab
Raika Khodadad <raika.khodadad@ticketmaster.com>
2019-01-23 19:51:23 -0800

    sorting by resolutiondate based on owner's suggestion

--- notify.go
+++ notify.go
@@ -171,7 +171,7 @@ func toIssueLabel(groupLabels alertmanager.KV) string {
 }
 
 func (r *Receiver) search(project, issueLabel string) (*jira.Issue, bool, error) {
-	query := fmt.Sprintf("project=\"%s\" and labels=%q order by key desc", project, issueLabel)
+	query := fmt.Sprintf("project=\"%s\" and labels=%q order by resolutiondate desc", project, issueLabel)
 	options := &jira.SearchOptions{
 		Fields:     []string{"summary", "status", "resolution", "resolutiondate"},
 		MaxResults: 2,
