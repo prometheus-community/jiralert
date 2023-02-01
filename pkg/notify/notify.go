c5bc6137cf08ff2b65bc985598242e1bc9bacc25
jzajic <jan.zajic@corpus.cz>
2023-02-01 19:59:57 +0100

    Fix #146 (safe limit of 200 characters from group label value)
    Signed-off-by: jzajic <jan.zajic@corpus.cz>

--- pkg/notify/notify.go
+++ pkg/notify/notify.go
@@ -176,7 +176,7 @@ func (r *Receiver) Notify(data *alertmanager.Data, hashJiraLabel bool) (bool, er
 
 	if r.conf.AddGroupLabels {
 		for k, v := range data.GroupLabels {
-			issue.Fields.Labels = append(issue.Fields.Labels, fmt.Sprintf("%s=%q", k, v))
+			issue.Fields.Labels = append(issue.Fields.Labels, fmt.Sprintf("%s=%.200q", k, v))
 		}
 	}
 
